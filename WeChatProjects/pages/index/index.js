b583148933b83dfd973533360d35c0e2a5393f60
Vincy SHI <vincy@vincy1230.net>
2024-09-14 21:42:20 +0800

    [upd] 老八食肆 主页完成

--- WeChatProjects/pages/index/index.js
+++ WeChatProjects/pages/index/index.js
@@ -2,11 +2,26 @@
 var pageObject = {
     data: {
         swpList: [
-            { img: '', text: 'qqqq1' },
-            { img: '', text: 'qqqq2' },
-            { img: '', text: 'qqqq3' },
-            { img: '', text: 'qqqq4' },
-            { img: '', text: 'qqqq5' },
+            {
+                img: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/Static/laoba1.jpg',
+                text: '孤独美食家倾情奉献',
+            },
+            {
+                img: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/Static/laoba2.jpg',
+                text: '传统自然生态用餐环境',
+            },
+            {
+                img: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/Static/laoba3.jpg',
+                text: '丰富的民俗演艺节目',
+            },
+            {
+                img: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/Static/laoba4.jpg',
+                text: '与资深老饕分享交流',
+            },
+            {
+                img: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/Static/laoba5.jpg',
+                text: '老八食肆提醒您，记得好好吃饭',
+            },
         ],
     },
     onShareAppMessage() {},
