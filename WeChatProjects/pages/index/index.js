2c4a840785854bd486871d53bef533a0ce5114e9
Vincy SHI <vincy@vincy1230.net>
2024-09-14 20:22:00 +0800

    [init] 建立工作树

--- WeChatProjects/pages/index/index.js
+++ WeChatProjects/pages/index/index.js
@@ -1,2 +1,14 @@
-// index.js
-Page({})
+// pages/index/index.js
+var pageObject = {
+    data: {
+        swpList: [
+            { img: '', text: 'qqqq1' },
+            { img: '', text: 'qqqq2' },
+            { img: '', text: 'qqqq3' },
+            { img: '', text: 'qqqq4' },
+            { img: '', text: 'qqqq5' },
+        ],
+    },
+    onShareAppMessage() {},
+};
+Page(pageObject);
