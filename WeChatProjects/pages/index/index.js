06e2312e8d15a2aa30e003a01fa7a46d1e869909
Vincy SHI <vincy@vincy1230.net>
2024-09-14 22:32:42 +0800

    [fix] 修改 banner 路径

--- WeChatProjects/pages/index/index.js
+++ WeChatProjects/pages/index/index.js
@@ -3,23 +3,23 @@ var pageObject = {
     data: {
         swpList: [
             {
-                img: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/Static/laoba1.jpg',
+                img: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/Static/banner/laoba1.jpg',
                 text: '孤独美食家倾情奉献',
             },
             {
-                img: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/Static/laoba2.jpg',
+                img: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/Static/banner/laoba2.jpg',
                 text: '传统自然生态用餐环境',
             },
             {
-                img: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/Static/laoba3.jpg',
+                img: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/Static/banner/laoba3.jpg',
                 text: '丰富的民俗演艺节目',
             },
             {
-                img: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/Static/laoba4.jpg',
+                img: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/Static/banner/laoba4.jpg',
                 text: '与资深老饕分享交流',
             },
             {
-                img: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/Static/laoba5.jpg',
+                img: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/Static/banner/laoba5.jpg',
                 text: '老八食肆提醒您，记得好好吃饭',
             },
         ],
