1e753186a8eeababc73b319212bdab4f86f807c8
Vincy SHI <vincy@vincy1230.net>
2024-09-20 15:31:43 +0800

    [add] bgm

--- WeChatProjects/pages/me/me.js
+++ WeChatProjects/pages/me/me.js
@@ -1,3 +1,19 @@
 // pages/me/me.js
-var pageObject = {};
+var pageObject = {
+    onLoad(query) {
+        this.dslb = wx.createInnerAudioContext({
+            useWebAudioImplement: false,
+        });
+        this.dslb.src =
+            'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/Static/岛市老八RAP.mp3';
+        this.dslb.loop = true;
+    },
+    onShow() {
+        this.dslb.play();
+    },
+    onHide() {
+        this.dslb.pause();
+    },
+};
+
 Page(pageObject);
