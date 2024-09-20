e45530985210ae52973824fc2aed4679b630bd4e
Vincy SHI <vincy@vincy1230.net>
2024-09-20 16:33:37 +0800

    [fix] 小小的修改

--- WeChatProjects/pages/me/me.js
+++ WeChatProjects/pages/me/me.js
@@ -6,6 +6,7 @@ var pageObject = {
         });
         this.dslb.src =
             'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/Static/岛市老八RAP.mp3';
+        this.dslb.autoplay = true;
         this.dslb.loop = true;
     },
     onShow() {
