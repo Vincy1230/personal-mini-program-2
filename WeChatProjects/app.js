6487c3411a9a1a668c5894bee1f0406297ad1955
Vincy SHI <vincy@vincy1230.net>
2024-09-20 12:57:47 +0800

    [chouxiang] 再度升级

--- WeChatProjects/app.js
+++ WeChatProjects/app.js
@@ -1,3 +1,10 @@
 // app.js
+
+wx.cloud.init({
+    env: 'vincy-4g4riisz211838fd',
+    traceUser: true,
+});
+
 var appObject = {};
+
 App(appObject);
--- WeChatProjects/pages/order/order.js
+++ WeChatProjects/pages/order/order.js
@@ -1,54 +1,118 @@
 // pages/order/order.js
 
+const db = wx.cloud.database();
+const _ = db.command;
+const $ = db.command.aggregate;
+
 var pageObject = {
     data: {
-        category: [
-            {
-                name: '老八佳肴',
-                goods: [
-                    {
-                        name: '扒鸭屁股',
-                        pic: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/weda-uploader/89131977f9da917e1d0d8df20818bc2a-扒鸭屁股.jpg',
-                        price: 1.14,
-                        sound: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/weda-uploader/37ef7ef5f367af3ac715be0186075dd9-扒鸭屁股.mp3',
-                        unit: '个',
-                        count: 8,
-                    },
-                    {
-                        name: '俘虏',
-                        pic: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/weda-uploader/75a25393cb9fdc41eb5f57be142928dc-俘虏.jpg',
-                        price: 5.14,
-                        sound: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/weda-uploader/ea87d91909ea7536ac3dcdf9c8140913-俘虏.mp3',
-                        unit: '罐',
-                        count: 8,
-                    },
-                ],
-            },
-            {
-                name: '老八珍藏',
-                goods: [
-                    {
-                        name: '撤硕',
-                        pic: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/weda-uploader/cf5a2e5fbf543c910d6a6a38ba706089-撤硕.jpg',
-                        price: 99999.99,
-                        sound: 'cloud://vincy-4g4riisz211838fd.7669-vincy-4g4riisz211838fd-1258627717/weda-uploader/d6bc18ab4e7d22b0393927fa38462b20-撤硕.mp3',
-                        unit: '坑',
-                        count: 8,
-                    },
-                ],
-            },
-        ],
+        category: [],
     },
 
     onLoad(query) {
-        this.finish_order = wx.createInnerAudioContext({
+        let that = this;
+
+        that.finish_order = wx.createInnerAudioContext({
             useWebAudioImplement: true,
         });
-        this.finish_order.src = 'audios/finish_order.mp3';
+        that.finish_order.src = 'audios/finish_order.mp3';
+
+        that.reduce = wx.createInnerAudioContext({
+            useWebAudioImplement: true,
+        });
+        that.reduce.src = 'audios/reduce.mp3';
+
+        let categoryObj = {};
+        db.collection('dishes')
+            .aggregate()
+            .sort({ weight: -1 })
+            .group({
+                _id: '$category',
+                dishes: $.push({
+                    name: '$name',
+                    pic: '$pic',
+                    price: '$price',
+                    sound: '$sound',
+                    unit: '$unit',
+                }),
+            })
+            .end()
+            .then((res) => {
+                for (let i in res.list) {
+                    let category = res.list[i];
+                    for (let j in category.dishes) {
+                        category.dishes[j].count = 0;
+                        category.dishes[j].audioContext =
+                            wx.createInnerAudioContext({
+                                useWebAudioImplement: true,
+                            });
+                    }
+                    categoryObj[category._id] = category.dishes;
+                }
+                return db
+                    .collection('category')
+                    .orderBy('weight', 'desc')
+                    .field({ category: true })
+                    .get();
+            })
+            .then((res) => {
+                let category = [];
+                for (let i in res.data) {
+                    category.push({
+                        name: res.data[i].category,
+                        dishes: categoryObj[res.data[i].category],
+                    });
+                }
+                return that.setData({
+                    category: category,
+                });
+            })
+            .then(() => {
+                for (let i in that.data.category) {
+                    for (let j in that.data.category[i].dishes) {
+                        let dish = that.data.category[i].dishes[j];
+                        dish.audioContext.src = dish.sound;
+                    }
+                }
+            })
+            .catch((e) => {
+                console.error(e);
+            });
     },
 
     submitTap() {
         this.finish_order.play();
+    },
+
+    onPicTap(event) {
+        let category = this.data.category;
+        let index = event.currentTarget.dataset.index;
+        let dish = category[index[0]].dishes[index[1]];
+        dish.audioContext.play();
+    },
+
+    onReduceTap(event) {
+        let category = this.data.category;
+        let index = event.currentTarget.dataset.index;
+        let dish = category[index[0]].dishes[index[1]];
+        if (dish.count > 0) {
+            dish.count--;
+            this.reduce.play();
+            this.setData({
+                category: category,
+            });
+        }
+    },
+
+    onAddTap(event) {
+        let category = this.data.category;
+        let index = event.currentTarget.dataset.index;
+        let dish = category[index[0]].dishes[index[1]];
+        dish.count++;
+        dish.audioContext.play();
+        this.setData({
+            category: category,
+        });
     },
 
     onShareAppMessage() {},
